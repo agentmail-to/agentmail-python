08d7d6510411d69e13d6c4215b54b67b5a29f433
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-18 23:02:56 +0000

    Release 0.0.44

--- src/agentmail/core/client_wrapper.py
+++ src/agentmail/core/client_wrapper.py
@@ -23,10 +23,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "agentmail/0.0.43",
+            "User-Agent": "agentmail/0.0.44",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "agentmail",
-            "X-Fern-SDK-Version": "0.0.43",
+            "X-Fern-SDK-Version": "0.0.44",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_api_key()}"

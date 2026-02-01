651168e37a6d58086c26144646ef62f6b6022635
André Abadesso <andre.abadesso@gmail.com>
2026-02-01 19:50:57 -0300

    fix: use nixos-25.11 stable channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "agentmail - Python SDK";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
